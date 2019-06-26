53a53f697910b1f3b234ce8d30592444558266f2
AnthonySkoury <askoury@uci.edu>
2019-06-26 12:32:30 -0700

    added graphs

--- app/src/main/java/com/example/sensorapp/MainActivity.java
+++ app/src/main/java/com/example/sensorapp/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.sensorapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class MainActivity extends AppCompatActivity {
 
     MyCanvas Screen;
+
+    LineGraphSeries<DataPoint> series;
 
 
     @Override
@@ -21,7 +27,22 @@ public class MainActivity extends AppCompatActivity {
 
 
         Screen = (MyCanvas) findViewById(R.id.MyCanvas);
+        CreatePlot();
 
+    }
+
+    protected void CreatePlot(){
+        double y,x;
+        x=-5.0;
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        series = new LineGraphSeries<DataPoint>();
+
+        for(int i=0; i<500; i++){
+            x=x+0.1;
+            y=Math.sin(x);
+            series.appendData(new DataPoint(x,y), true, 500);
+        }
+        graph.addSeries(series);
     }
 
 
