284076f3851cb6595adab4ec739bcc9b933eb980
AnthonySkoury <askoury@uci.edu>
2019-07-09 11:12:43 -0700

    graph v1

--- app/src/main/java/com/example/sensorapp/DataSaver.java
+++ app/src/main/java/com/example/sensorapp/DataSaver.java
@@ -34,6 +34,10 @@ public class DataSaver {
         return position.size();
     }
 
+    public Vector<double[]> getPosition(){
+        return position;
+    }
+
     public int SaveToFile(){
 
         return 0;
--- app/src/main/java/com/example/sensorapp/PositionDisplay.java
+++ app/src/main/java/com/example/sensorapp/PositionDisplay.java
@@ -1,11 +1,18 @@
 package com.example.sensorapp;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.util.AttributeSet;
+import android.util.Log;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.PointsGraphSeries;
+
+import java.util.Vector;
+
+import static android.content.ContentValues.TAG;
 
 /* Simple trajectory plot for x and y position */
 public class PositionDisplay extends GraphView {
@@ -14,6 +21,8 @@ public class PositionDisplay extends GraphView {
     LineGraphSeries<DataPoint> xy_coord = new LineGraphSeries<>();
     LineGraphSeries<DataPoint> x_coord = new LineGraphSeries<>();
     LineGraphSeries<DataPoint> y_coord = new LineGraphSeries<>();
+
+    PointsGraphSeries<DataPoint> xySeries;
 
     public PositionDisplay(Context context) {
         super(context);
@@ -49,6 +58,44 @@ public class PositionDisplay extends GraphView {
 
     public void plotYPos(){
 
+    }
+
+    public void plotXY(Vector<double[]> position){
+
+        xySeries = new PointsGraphSeries<>();
+
+        for(int i = 0;i <position.size(); i++){
+            try{
+                double x = position.get(i)[0];
+                double y = position.get(i)[1];
+                xySeries.appendData(new DataPoint(x,y),true, maxDataPoints);
+            }catch (IllegalArgumentException e){
+                Log.e(TAG, "createScatterPlot: IllegalArgumentException: " + e.getMessage() );
+            }
+        }
+
+        //set some properties
+        xySeries.setShape(PointsGraphSeries.Shape.RECTANGLE);
+        xySeries.setColor(Color.BLUE);
+        xySeries.setSize(20f);
+
+        //set Scrollable and Scaleable
+        this.getViewport().setScalable(true);
+        this.getViewport().setScalableY(true);
+        this.getViewport().setScrollable(true);
+        this.getViewport().setScrollableY(true);
+
+        //set manual x bounds
+        this.getViewport().setYAxisBoundsManual(true);
+        this.getViewport().setMaxY(150);
+        this.getViewport().setMinY(-150);
+
+        //set manual y bounds
+        this.getViewport().setXAxisBoundsManual(true);
+        this.getViewport().setMaxX(150);
+        this.getViewport().setMinX(-150);
+
+        this.addSeries(xySeries);
     }
 
 }
